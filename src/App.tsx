1e20d99c1f8440e208da6bc70bd7eea860f1573f
tsteelematc <tsteele@madisoncollege.edu>
2025-02-05 17:49:08 -0600

    Some initial...
    Colors then spacing... padding margin...

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,28 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div
+      className=''
+    >
       <h1
-        className='text-2xl font-bold'
+        className='text-2xl font-bold text-secondary bg-base-300 p-4'
       >
         TCA Five Crowns
-      </h1>      
-      <button
-        className='btn btn-secondary btn-soft btn-xl'
+      </h1>
+      <div
+        className='p-4'
       >
-        Play Five Crowns
-      </button>
+        <button
+          className='btn btn-secondary btn-soft btn-xl'
+        >
+          Play Five Crowns
+        </button>
+        <h2
+          className='text-xl font-semi-bold mt-4'
+        >
+          Leaderboard
+        </h2>
+      </div>
     </div>
   )
 }
