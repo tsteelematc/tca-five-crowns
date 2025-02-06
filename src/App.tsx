e35afb8c3acef847c765f3dfa4353a5e236f605e
tsteelematc <tsteele@madisoncollege.edu>
2025-02-06 08:45:29 -0600

    And responsive grid for fun fact cards...
    This is so much fun ! ! !

--- src/App.tsx
+++ src/App.tsx
@@ -27,128 +27,133 @@ function App() {
         >
           Leaderboard
         </h2>
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
+        <div className="className grid gap-4 grid-cols-1 sm:grid-cols-2">
 
 
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <p>
-              Leadeboard goes here...
-            </p>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            {/* For TSX uncomment the commented types below */}
-            <span 
-              className="countdown font-mono text-6xl"
-              onClick={() => setCountdownNumber(countdownNumber - 1)}
-            >
-              <span style={{ "--value": countdownNumber } as React.CSSProperties}>{countdownNumber}</span>
-            </span>
-          </div>
-        </div>
-
-
-        <div
-          className="card w-full bg-base-100 card-lg shadow-xl mt-4">
-          <div className="card-body">
-            <h2 className="card-title">Leaderboard</h2>
-            <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
-            </div>
-            <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
-            </div>
-            <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
-            </div>
-            <div className="chat chat-start">
-              <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
-            </div>
-            <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
-            </div>
-            <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
-            </div>
-            <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
-            </div>
-            <div className="chat chat-end">
-              <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
             </div>
           </div>
-        </div>
 
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <p>
+                Leadeboard goes here...
+              </p>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              {/* For TSX uncomment the commented types below */}
+              <span
+                className="countdown font-mono text-6xl"
+                onClick={() => setCountdownNumber(countdownNumber - 1)}
+              >
+                <span style={{ "--value": countdownNumber } as React.CSSProperties}>{countdownNumber}</span>
+              </span>
+            </div>
+          </div>
+
+
+          <div
+            className="card w-full bg-base-100 card-lg shadow-xl mt-4">
+            <div className="card-body">
+              <h2 className="card-title">Leaderboard</h2>
+              <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+              </div>
+              <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+              </div>
+              <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
+              </div>
+              <div className="chat chat-start">
+                <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+              </div>
+              <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+              </div>
+              <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+              </div>
+              <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+              </div>
+              <div className="chat chat-end">
+                <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
 
 
       </div>
