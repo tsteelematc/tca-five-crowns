0e74b4d0eb78989063c0b8ea558135f637a53b4c
tsteelematc <tsteele@madisoncollege.edu>
2025-02-06 13:00:23 -0600

    Learned about padding and margins...
    Also saw some font, color Tailwind and DasiyUI styling...

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ function App() {
   return (
     <div>
       <h1
-        className='text-2xl font-bold'
+        className='text-2xl font-bold bg-base-300 p-4 text-secondary'
       >
         TCA Five Crowns
-      </h1>      
-      <button
-        className='btn btn-secondary btn-soft btn-xl'
+      </h1>
+      <div
+        className="p-4"
       >
-        Play Five Crowns
-      </button>
+        <button
+          className='btn btn-secondary btn-soft btn-xl'
+        >
+          Play Five Crowns
+        </button>
+        <h2
+          className='mt-3 text-xl font-semi-bold'
+        >
+          Leaderboard...
+        </h2>
+      </div>
     </div>
   )
 }
