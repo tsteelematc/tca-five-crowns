2a95d635d93ad7bcff0bdf066d477a7585af788d
tsteelematc <tsteele@madisoncollege.edu>
2025-02-06 13:16:33 -0600

    Played with daisy ui chat bubbles...
    Neato...

--- src/App.tsx
+++ src/App.tsx
@@ -27,13 +27,13 @@ function App() {
 
 
 
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -48,13 +48,13 @@ function App() {
 
 
 
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -69,13 +69,13 @@ function App() {
 
 
 
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -88,33 +88,13 @@ function App() {
 
 
 
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
-              className="card-title"
-            >
-              Leaderboard...
-            </h2>
-            <p>
-              Leaderboard coming soon...
-            </p>
-          </div>
-        </div>
-
-
-
-
-        <div 
-          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
-        >
-          <div 
-            className="card-body"
-          >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -128,14 +108,13 @@ function App() {
 
 
 
-
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -150,14 +129,13 @@ function App() {
 
 
 
-
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
-          <div 
+          <div
             className="card-body"
           >
-            <h2 
+            <h2
               className="card-title"
             >
               Leaderboard...
@@ -165,6 +143,59 @@ function App() {
             <p>
               Leaderboard coming soon...
             </p>
+          </div>
+        </div>
+
+
+
+
+
+
+        <div
+          className="card w-full bg-base-100 card-lg shadow-sm mt-4"
+        >
+          <div
+            className="card-body"
+          >
+            <h2
+              className="card-title"
+            >
+              Leaderboard...
+            </h2>
+
+
+
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+            </div>
+
+
+
+
+
+
+
           </div>
         </div>
 
