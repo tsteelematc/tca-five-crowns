67f89e8cceb7fc8b987d286fd141454efefd3540
tsteelematc <tsteele@madisoncollege.edu>
2025-02-24 14:54:54 -0600

    With var, not state, blah, blah, blah...

--- src/Play.tsx
+++ src/Play.tsx
@@ -4,6 +4,8 @@ export const Play = () => {
 
     const nav = useNavigate();
 
+    let turnNumber = 1;
+
     return (
         <>
             <h3
@@ -11,6 +13,22 @@ export const Play = () => {
             >
                 Play
             </h3>
+            <h4 className="text-lg font-semibold">
+                Turn # {turnNumber}
+                <button 
+                    className="btn btn-xs btn-outline btn-light ml-4"
+                    onClick={
+                        // () => turnNumber = turnNumber + 1
+                        () => {
+                            console.log(turnNumber);
+                            turnNumber = turnNumber + 1;
+                            console.log(turnNumber);
+                        }
+                    }
+                >
+                    +
+                </button>
+            </h4>
             <button
                 className="btn btn-active btn-secondary btn-lg mt-4"
                 onClick={
