5a0dc60776fd4471d5861fb54f6eccfcef601e56
tsteelematc <tsteele@madisoncollege.edu>
2025-02-26 19:15:24 -0600

    Leaderboard mostly done...
    What if no games ? ? ?

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router";
+import { LeaderboardEntry } from "./GameResults";
 
 interface HomeProps {
     totalGameCount: number;
+    leaderboardData: LeaderboardEntry[];
 };
 
 export const Home: React.FC<HomeProps> = ({
     totalGameCount
+    , leaderboardData
 }) => {
 
     // Use a react hook for button navigation...
@@ -26,6 +29,54 @@ export const Home: React.FC<HomeProps> = ({
             >
                 Play Five Crowns
             </button>
+
+            <div
+                className="card w-full bg-base-100 card-md shadow-xl"
+            >
+                <div
+                    className="card-body"
+                >
+                    <h2
+                        className="card-title"
+                    >
+                        Leaderboard
+                    </h2>
+                    <table
+                        className="table"
+                    >
+                        <thead>
+                            <tr>
+                                <th>W</th>
+                                <th>L</th>
+                                <th>AVG</th>
+                                <th>PLAYER</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                leaderboardData.map(
+                                    x => (
+                                        <tr>
+                                            <td>
+                                                {x.wins}
+                                            </td>
+                                            <td>
+                                                {x.losses}
+                                            </td>
+                                            <td>
+                                                {x.average}
+                                            </td>
+                                            <td>
+                                                {x.player}
+                                            </td>
+                                        </tr>
+                                    )
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     );
 };
