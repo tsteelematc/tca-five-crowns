a557dedfb41d1049867e8c1498d8649535daa14b
tsteelematc <tsteele@madisoncollege.edu>
2025-03-04 13:41:37 -0600

    Done with misc/header-with-dynamic-title
    Boom ! ! !

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 interface SetupProps {
@@ -8,7 +9,10 @@ export const Setup: React.FC<SetupProps> = ({
     setTitle
 }) => {
 
-    setTitle("Setup");
+    useEffect(
+        () => setTitle("Setup")
+        , []
+    );
 
     const foobarcat = useNavigate();
 
