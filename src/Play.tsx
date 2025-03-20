30e54feaa1099f6d178698bd49131bad69cc4efb
tsteelematc <tsteele@madisoncollege.edu>
2025-03-20 13:21:40 -0500

    Some instructions for entering scores...
    And alignment changes...
    
    Pixel perfect : - O
    
    Maybe...

--- src/Play.tsx
+++ src/Play.tsx
@@ -168,11 +168,22 @@ export const Play: React.FC<PlayProps> = ({
 
     return (
         <>
+            <p>
+                Press
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 inline mx-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                buttons (below) to enter scores for each hand...
+            </p>
             <div className="overflow-x-auto mt-4">
                 <table className="table table-lg table-pin-rows table-pin-cols">
                     <thead>
                         <tr>
-                            <th></th>
+                            <td
+                                className="font-light"
+                            >
+                                Wild Card
+                            </td>
                             {
                                 dummyPlayers.map(
                                     x => (
@@ -196,7 +207,7 @@ export const Play: React.FC<PlayProps> = ({
                             whildCardHands.map(
                                 x => (
                                     <tr
-                                        className="text-center"
+                                        className="text-left"
                                     >
                                         <th
                                             className="font-normal text-sm"
