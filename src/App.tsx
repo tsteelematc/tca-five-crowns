acbef62de7f96d429433586e2ba662f5635c5a3a
tsteelematc <tsteele@madisoncollege.edu>
2025-04-04 15:01:04 +0000

    Smaller light/dark button, from codspaces and ipad mini : - )

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,7 @@ const App = () => {
 
           {/* sun icon */}
           <svg
-            className="swap-on h-8 w-8 fill-current"
+            className="swap-on h-6 w-6 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24">
             <path
@@ -139,7 +139,7 @@ const App = () => {
 
           {/* moon icon */}
           <svg
-            className="swap-off h-8 w-8 fill-current"
+            className="swap-off h-6 w-6 fill-current"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24">
             <path
