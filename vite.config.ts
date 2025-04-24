a8d6e16de647d55f64e3be66c1b2a4104c18b81a
tsteelematc <tsteele@madisoncollege.edu>
2025-04-24 15:26:19 -0500

    Worked better on Mac...
    Hmm, need to see how to run on arm64 Windows : - O
    
    Learning is fun ! ! !

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Five Crowns Companion',
+        short_name: '5-crowns',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }, 
+    })
   ],
   build: {
     outDir: "docs"
